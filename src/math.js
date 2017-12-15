0ecc27fb0677174e934efc6d8813441ac693b007
Nate Clark <n8@n8io.com>
2017-12-15 14:50:09 -0500

    Add multiply

--- src/math.js
+++ src/math.js
@@ -1,10 +1,12 @@
 const isNumber = num => Number.isNaN(Number(num)) === false;
 const additionReducer = (acc, num) => acc + Number(num);
 const subtractionReducer = (acc, num) => acc - Number(num);
+const multiplyReducer = (acc, num) => acc * Number(num);
 
 const mathFunctor = (fn, ...rest) => [...rest].filter(isNumber).reduce(fn, 0);
 
 const add = (...numbers) => mathFunctor(additionReducer, ...numbers);
 const subtract = (...numbers) => mathFunctor(subtractionReducer, ...numbers);
+const multiply = (...numbers) => mathFunctor(multiplyReducer, ...numbers);
 
-export { add, subtract };
+export { add, subtract, multiply };
